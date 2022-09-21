936ec9eec7fe2d5ccfef800e4fe01f7644f12f72
Pablo Frank <jgfpablo@gmail.com>
2022-09-20 23:49:20 -0300

    Desafío: Sincronizar counter

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,30 +7,32 @@ const Cart = () => {
     const { state } = useContext(ContextCart);
     console.log(state);
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>nombre</th>
-                    <th>imagen</th>
-                    <th>precio</th>
-                    <th>cantidad</th>
-                </tr>
-            </thead>
-            <tbody>
-                {state.map((stat, index) => {
-                    return (
-                        <tr>
-                            <td>{stat.nombre}</td>
-                            <td>
-                                <img src={stat.imagen} />
-                            </td>
-                            <td>{stat.precio}</td>
-                            <td>{stat.cantidad}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+        <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>nombre</th>
+                        <th>imagen</th>
+                        <th>precio</th>
+                        <th>cantidad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {state.map((stat, index) => {
+                        return (
+                            <tr key={index}>
+                                <td>{stat.nombre}</td>
+                                <td>
+                                    <img src={stat.imagen} />
+                                </td>
+                                <td>{stat.precio}</td>
+                                <td>{stat.cantidad}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
